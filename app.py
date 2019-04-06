c38d424e81203b9e6190474592a11f691aa3c2fa
EdgarDominguezRamos <1717110264@utectulancingo.edu.mx>
2019-04-05 19:21:05 -0600

    url mapeadas para post y usuarios_eco

--- app.py
+++ app.py
@@ -28,6 +28,16 @@ urls = (
     '/clientes/delete/(.+)', 'application.controllers.clientes.delete.Delete',
     '/clientes/insert', 'application.controllers.clientes.insert.Insert',
     '/api_clientes/?', 'application.api.clientes.api_clientes.Api_clientes',
+    '/post', 'application.controllers.post.index.Index',
+    '/post/view/(.+)', 'application.controllers.post.view.View',
+    '/post/edit/(.+)', 'application.controllers.post.edit.Edit',
+    '/post/delete/(.+)', 'application.controllers.post.delete.Delete',
+    '/post/insert', 'application.controllers.post.insert.Insert',
+    '/usuarios_eco', 'application.controllers.usuarios_eco.index.Index',
+    '/usuarios_eco/view/(.+)', 'application.controllers.usuarios_eco.view.View',
+    '/usuarios_eco/edit/(.+)', 'application.controllers.usuarios_eco.edit.Edit',
+    '/usuarios_eco/delete/(.+)', 'application.controllers.usuarios_eco.delete.Delete',
+    '/usuarios_eco/insert', 'application.controllers.usuarios_eco.insert.Insert',
 )
 
 app = web.application(urls, globals())
