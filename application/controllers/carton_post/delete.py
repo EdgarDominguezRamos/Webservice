b3e78cc93a3296fbcd36377aeb70906ec28d0af0
EdgarDominguezRamos <1717110264@utectulancingo.edu.mx>
2019-04-24 17:24:10 -0500

    delete con login

--- application/controllers/carton_post/delete.py
+++ application/controllers/carton_post/delete.py
@@ -7,7 +7,7 @@ class Delete:
     def __init__(self):
         pass
 
-    '''
+    
     def GET(self, id_carton_post, **k):
         if app.session.loggedin is True: # validate if the user is logged
             # session_username = app.session.username
@@ -30,21 +30,17 @@ class Delete:
         else: # the user dont have logged
             raise config.web.seeother('/login') # render login.html
 
+
     @staticmethod
     def GET_DELETE(id_carton_post, **k):
-
-    @staticmethod
-    def POST_DELETE(id_carton_post, **k):
-    '''
-
-    def GET(self, id_carton_post, **k):
         message = None # Error message
         id_carton_post = config.check_secure_val(str(id_carton_post)) # HMAC id_carton_post validate
         result = config.model.get_carton_post(int(id_carton_post)) # search  id_carton_post
         result.id_carton_post = config.make_secure_val(str(result.id_carton_post)) # apply HMAC for id_carton_post
         return config.render.delete(result, message) # render delete.html with user data
 
-    def POST(self, id_carton_post, **k):
+    @staticmethod
+    def POST_DELETE(id_carton_post, **k):
         form = config.web.input() # get form data
         form['id_carton_post'] = config.check_secure_val(str(form['id_carton_post'])) # HMAC id_carton_post validate
         result = config.model.delete_carton_post(form['id_carton_post']) # get carton_post data
--- application/controllers/pet_post/delete.py
+++ application/controllers/pet_post/delete.py
@@ -7,7 +7,7 @@ class Delete:
     def __init__(self):
         pass
 
-    '''
+    
     def GET(self, id_pet_post, **k):
         if app.session.loggedin is True: # validate if the user is logged
             # session_username = app.session.username
@@ -30,21 +30,17 @@ class Delete:
         else: # the user dont have logged
             raise config.web.seeother('/login') # render login.html
 
+
     @staticmethod
     def GET_DELETE(id_pet_post, **k):
-
-    @staticmethod
-    def POST_DELETE(id_pet_post, **k):
-    '''
-
-    def GET(self, id_pet_post, **k):
         message = None # Error message
         id_pet_post = config.check_secure_val(str(id_pet_post)) # HMAC id_pet_post validate
         result = config.model.get_pet_post(int(id_pet_post)) # search  id_pet_post
         result.id_pet_post = config.make_secure_val(str(result.id_pet_post)) # apply HMAC for id_pet_post
         return config.render.delete(result, message) # render delete.html with user data
 
-    def POST(self, id_pet_post, **k):
+    @staticmethod
+    def POST_DELETE(id_pet_post, **k):
         form = config.web.input() # get form data
         form['id_pet_post'] = config.check_secure_val(str(form['id_pet_post'])) # HMAC id_pet_post validate
         result = config.model.delete_pet_post(form['id_pet_post']) # get pet_post data
